f72eb4d98ea2840005b102524531a9b27e5911a0
Lee-sunghawn99 <89784307+Lee-sunghawn99@users.noreply.github.com>
2021-09-09 14:50:12 +0900

    Add files via upload

--- prof/move.py
+++ prof/move.py
@@ -24,10 +24,40 @@ def move_base_to(x, y):
     goal.target_pose.header.stamp = rospy.Time.now()
     goal.target_pose.pose.position.x = x
     goal.target_pose.pose.position.y = y
-    goal.target_pose.pose.orientation.x = 0.0
-    goal.target_pose.pose.orientation.y = 0.0
-    goal.target_pose.pose.orientation.z = 0.0
-    goal.target_pose.pose.orientation.w = 1.0
+    if x == 0.039 and y == 0.044:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.670
+        goal.target_pose.pose.orientation.w = 0.742
+    if x == 0.130 and y == 1.636:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.717
+        goal.target_pose.pose.orientation.w = 0.698
+    if x == 0.533 and y == 1.636:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.250
+        goal.target_pose.pose.orientation.w = 1.000
+    if x == 0.673 and y == 0.392:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = -0.699
+        goal.target_pose.pose.orientation.w = 0.716
+    if x == 1.221 and y == 0.392:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.721
+        goal.target_pose.pose.orientation.w = 0.693
+    if x == 1.282 and y == 1.939:
+        goal.target_pose.pose.orientation.x = 0.0
+        goal.target_pose.pose.orientation.y = 0.0
+        goal.target_pose.pose.orientation.z = 0.693
+        goal.target_pose.pose.orientation.w = 0.721
+    #goal.target_pose.pose.orientation.x = 0.0
+    #goal.target_pose.pose.orientation.y = 0.0
+    #goal.target_pose.pose.orientation.z = 0.708
+    #goal.target_pose.pose.orientation.w = 1.0
     rospy.loginfo("Sending goal")
     client.send_goal(goal)
     wait = client.wait_for_result()
@@ -40,13 +70,12 @@ def move_base_to(x, y):
 
 
 if __name__ == '__main__':
-    goal_positions =[[0.037, -0.020],
-                    [0.064,  1.501],
-                    [0.547,  1.600],
-                    [0.609,  0.531],
-                    [1.174,  0.367],
-                    [1.144,  0.405],
-                    [1.313, 2.045]]
+    goal_positions =[[0.039, 0.044],
+                    [0.130,  1.636],
+                    [0.533,  1.636],
+                    [0.673,  0.392],
+                    [1.221,  0.392],
+                    [1.282, 1.939]]
     rospy.init_node("set_navigation_goal")
 
     for x, y in goal_positions:
