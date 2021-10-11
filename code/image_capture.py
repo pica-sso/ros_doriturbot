036cf33149a9b0903d20cec6be3541bf7fc54af6
Lee-sunghawn99 <89784307+Lee-sunghawn99@users.noreply.github.com>
2021-10-11 17:44:34 +0900

    Add files via upload

--- code/image_capture.py
+++ code/image_capture.py
@@ -10,8 +10,8 @@ import cv2
 import rospy
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs import CompressedImage
-from nav_msg.msg import Odometry
+from sensor_msgs.msg import CompressedImage
+from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
 class ImageCapture:
@@ -19,7 +19,7 @@ class ImageCapture:
         self.pos_x = 3.3
         self.pos_y = 7.7
         self.theta = 3.14
-        self.image = np.empty((480, 640), dtype=np.unit8)
+        self.image = np.empty((480, 640), dtype=np.uint8)
         self.pub = rospy.Publisher('rpi_image', CompressedImage, queue_size=1)
         self.sub = rospy.Subscriber('odom', Odometry, self.update_pose)
 
@@ -50,19 +50,19 @@ class ImageCapture:
         except CvBridgeError as e:
             print(e)
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = f'x:{self.pos_x:.2f}_y:{self.pos_y:.2f}_a{self.theta:.2f}'
+        msg.header.frame_id = 'x:{self.pos_x:.2f}_y:{self.pos_y:.2f}_a{self.theta:.2f}'
         self.pub.publish(msg)
         rospy.loginfo("image was published")
 
     def run(self, capture_count=10.0, publish_count=50, duration=1.0):
-        cap_timer = rospt.Timer(rospy.Duration(duration / capture_count), self.capture_image)
+        cap_timer = rospy.Timer(rospy.Duration(duration / capture_count), self.capture_image)
         pub_timer = rospy.Timer(rospy.Duration(duration / capture_count), self.capture_image)
         rospy.sleep(duration)
         cap_timer.shutdown()
         pub_timer.shutdown()
 
-   if __name__ == "__main__":
-        rospy.init_node("image_capture")
-        image_capture = ImageCapture()
-        image_capture.run(capture_count=10.0, publish_count=5.0, duration=5.0)
-        rospy.spin()
+if __name__ == "__main__":
+    rospy.init_node("image_capture")
+    image_capture = ImageCapture()
+    image_capture.run(capture_count=10.0, publish_count=5.0, duration=5.0)
+    rospy.spin()
